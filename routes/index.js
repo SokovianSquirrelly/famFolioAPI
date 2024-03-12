29f3fc9bcaf9c8c35513a6b7adcd6a25f6ff5d9c
cal20014 <cal20014@byui.edu>
2024-03-12 13:15:33 -0600

    Update package dependencies and add authorization middleware

--- routes/index.js
+++ routes/index.js
@@ -1,30 +1,26 @@
 const express = require("express");
 const router = express.Router();
-const swagger = require("./swaggerRoutes");
+const { auth } = require("express-oauth2-jwt-bearer");
 
-// Import routes
 const userRoutes = require("./userRoutes");
 const accountTypeRoutes = require("./accountTypeRoutes");
 const categoryRoutes = require("./categoryRoutes");
 const genreRoutes = require("./genreRoutes");
 const mediaRoutes = require("./mediaRoutes");
 
-// Routes
-router.use("/user", userRoutes);
-router.use("/accountType", accountTypeRoutes);
-router.use("/category", categoryRoutes);
-router.use("/genre", genreRoutes);
-router.use("/media", mediaRoutes);
-router.use("/api-docs", swagger);
-
-// Error handling middleware
-router.use((err, req, res, next) => {
-  console.error(err.stack);
-  if (process.env.NODE_ENV === "development") {
-    res.status(500).send(err.stack);
-  } else {
-    res.status(500).send("Something went wrong");
-  }
+// Authorization middleware. When used, the Access Token must
+// exist and be verified against the Auth0 JSON Web Key Set.
+const checkJwt = auth({
+  audience: "{yourApiIdentifier}",
+  issuerBaseURL: `https://dev-ofqugjdj7o7861mc.us.auth0.com/`,
 });
+
+// Routes
+router.use("/user", checkJwt, userRoutes);
+router.use("/accountType", checkJwt, accountTypeRoutes);
+router.use("/category", checkJwt, categoryRoutes);
+router.use("/genre", checkJwt, genreRoutes);
+router.use("/media", checkJwt, mediaRoutes);
+router.use("/api-docs", swagger);
 
 module.exports = router;
