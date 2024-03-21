3995cf868c61062316ec911d3416e4be69bb2c64
cal20014 <cal20014@byui.edu>
2024-03-21 14:43:29 -0600

    Fix swaggerRoutes import issue

--- routes/swaggerRoutes.js
+++ routes/swaggerRoutes.js
@@ -1,7 +1,8 @@
 import express from "express";
-const router = express.Router();
 import swaggerUi from "swagger-ui-express";
 import swaggerDocument from "../swagger.json" assert { type: "json" };
+
+const router = express.Router();
 
 if (process.env.NODE_ENV === "development") {
   swaggerDocument.host = "localhost:8080";
