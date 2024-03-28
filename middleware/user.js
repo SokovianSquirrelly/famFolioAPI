731f2861f854ee09aebfad835894eb6c589027df
cal20014 <cal20014@byui.edu>
2024-03-28 14:51:02 -0600

    Add checkUserMediaAccess middleware function

--- middleware/user.js
+++ middleware/user.js
@@ -42,4 +42,32 @@ function checkAdmin(req, res, next) {
   }
 }
 
-export { checkUser, checkManager, checkAdmin };
+import Media from "../models/mediaModel.js";
+
+async function checkUserMediaAccess(req, res, next) {
+  try {
+    // Get the media ID from the request parameters
+    const mediaId = req.params.mediaId;
+
+    // Find the media by its ID
+    const media = await Media.findById(mediaId);
+
+    // If the media doesn't exist, send a 404 response
+    if (!media) {
+      return res.status(404).send("Media not found");
+    }
+
+    // If the user ID doesn't match the media's user ID, send a 403 response
+    if (req.user.userId !== media.userId) {
+      return res.status(403).send("Forbidden access");
+    }
+
+    // If the user has access to the media, continue to the next middleware function
+    next();
+  } catch (error) {
+    // If an error occurred, send a 500 response
+    res.status(500).send("Server error");
+  }
+}
+
+export { checkUser, checkManager, checkAdmin, checkUserMediaAccess };
